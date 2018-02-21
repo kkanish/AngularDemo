7e11d8631bb7993f0ad0634eab282c01a7ac2760
kkanish <kkanish@gmail.com>
2018-02-21 10:48:14 -0500

    collection component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
 
+import { MatListModule, MatCardModule, MatSlideToggleModule, MatDialogModule,
+ MatIconModule, MatInputModule, MatSnackBarModule, MatTabsModule,
+ MatButtonModule,MatLineModule, MatToolbarModule } from '@angular/material';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CollectionComponent } from './collection/collection.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CollectionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatListModule,
+    MatTabsModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    MatCardModule,MatIconModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatLineModule,
+    MatInputModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
